5b0a7bbb8dbce683c00255bb9a34d966cbe631ef
Daiki Matoba <telnetstat@gmail.com>
2018-07-13 19:14:38 +0900

    Make finder more safe

--- app/jobs/execute_crawler_job.rb
+++ app/jobs/execute_crawler_job.rb
@@ -1,6 +1,5 @@
 class ExecuteCrawlerJob < ApplicationJob
   def perform(crawler_job_id)
-    crawler_job = CrawlerJob.find(crawler_job_id)
-    crawler_job.execute_crawling
+    CrawlerJob.find_by(id: crawler_job_id)&.execute_crawling
   end
 end
