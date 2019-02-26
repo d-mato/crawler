b065eccbcba0fca78dc8be2f38c3b83351ef2eb2
Daiki Matoba <telnetstat@gmail.com>
2019-02-26 15:28:46 +0900

    Fix schema for mysql

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  after_initialize do
+    # not nullなtext型に空文字を設定
+    self.class.columns.each do |column|
+      if column.type == :text && column.null == false
+        self[column.name] ||= ''
+      end
+    end
+  end
 end
