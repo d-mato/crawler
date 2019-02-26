5cd129456938b5d48d639fe67985b07f1ca801c1
Daiki Matoba <telnetstat@gmail.com>
2019-02-26 15:16:39 +0900

    Replace db from sqlite to mysql

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :application, 'crawler'
 set :repo_url, 'git@github.com:d-mato/crawler.git'
 set :deploy_to, '/home/ec2-user/crawler'
 
-append :linked_files, 'db/production.sqlite3', 'config/master.key'
+append :linked_files, 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'storage', 'public/system'
