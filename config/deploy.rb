e33d6d425b73a510a5ced328c73cd704442705ef
Daiki Matoba <telnetstat@gmail.com>
2019-06-25 12:26:17 +0900

    Execute ridgepole after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,5 @@ set :deploy_to, '/home/ec2-user/crawler'
 
 append :linked_files, 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'storage', 'public/system'
+
+after 'deploy:publishing', 'ridgepole:apply'
