cc7e77269b9cc95118fefe0a9d1649b948dbd46c
Daiki Matoba <telnetstat@gmail.com>
2019-08-07 03:54:32 +0900

    Set up exception notifier

--- app/jobs/execute_crawler_job.rb
+++ app/jobs/execute_crawler_job.rb
@@ -1,5 +1,8 @@
 class ExecuteCrawlerJob < ApplicationJob
   def perform(crawler_job_id)
     CrawlerJob.find_by(id: crawler_job_id)&.execute_crawling
+  rescue => e
+    ExceptionNotifier.notify_exception(e)
+    raise e
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.middleware.use ExceptionNotification::Rack,
+                                          slack: {
+                                            webhook_url: Rails.application.credentials.dig(:slack, :webhook_url),
+                                            username: 'crawler',
+                                            additional_parameters: {
+                                              mrkdwn: true
+                                            },
+                                            additional_fields: [
+                                              { title: 'Mention', value: '<!channel>' }
+                                            ]
+                                          }
 end
